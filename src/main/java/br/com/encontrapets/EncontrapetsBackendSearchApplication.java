b5218fbd8ad56618817a364c65a53f9fcdb59dcc
Bruno Alves Justino <brunojustino@gmail.com>
2024-11-20 00:44:55 -0300

    Fix entities.

--- src/main/java/br/com/encontrapets/EncontrapetsBackendSearchApplication.java
+++ src/main/java/br/com/encontrapets/EncontrapetsBackendSearchApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EncontrapetsBackendAuthApplication {
+public class EncontrapetsBackendSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EncontrapetsBackendAuthApplication.class, args);
+		SpringApplication.run(EncontrapetsBackendSearchApplication.class, args);
+		
 	}
 
 }
